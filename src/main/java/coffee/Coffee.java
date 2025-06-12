2c9d40a0cbbe3511746f6c41ef97bfa1a6ac27a3
nyximos <nyximos@gmail.com>
2025-06-13 00:46:51 +0900

    feat: 커피 도메인 클래스 수정

--- src/main/java/coffee/Coffee.java
+++ src/main/java/coffee/Coffee.java
@@ -1,9 +1,43 @@
 package coffee;
 
-import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Getter
+@AllArgsConstructor
 public class Coffee {
     private String name;
     private String description;
     private BigDecimal price;
+
+    public Coffee(String name, int price) {
+        this.name = name;
+        this.price = BigDecimal.valueOf(price);
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuilder(name)
+                .append(" ")
+                .append(price)
+                .append("원 ")
+                .toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coffee coffee = (Coffee) o;
+        return Objects.equals(name, coffee.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
+    }
+
 }
